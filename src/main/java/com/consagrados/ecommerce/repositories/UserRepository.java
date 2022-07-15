d00f0dd0294787d1f610ece458bea8a653ddceff
AlanJSimpson <allanstark2908@gmail.com>
2022-07-15 16:26:42 -0300

    Service layer, component registration

--- src/main/java/com/consagrados/ecommerce/repositories/UserRepository.java
+++ src/main/java/com/consagrados/ecommerce/repositories/UserRepository.java
@@ -1,7 +1,8 @@
 package com.consagrados.ecommerce.repositories;
 
-import com.consagrados.ecommerce.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.consagrados.ecommerce.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
