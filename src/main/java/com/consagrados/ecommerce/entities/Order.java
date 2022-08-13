7af8fb62b2584fdb17059d2fbdddb40a7000d844
AlanJSimpson <allanstark2908@gmail.com>
2022-08-13 17:41:40 -0300

    subtotal & total methods

--- src/main/java/com/consagrados/ecommerce/entities/Order.java
+++ src/main/java/com/consagrados/ecommerce/entities/Order.java
@@ -41,7 +41,7 @@ public class Order implements Serializable {
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 
-	@OneToOne(mappedBy = "order" , cascade = CascadeType.ALL)
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 
 	public Order() {
@@ -99,6 +99,15 @@ public class Order implements Serializable {
 
 	public Set<OrderItem> getItems() {
 		return items;
+	}
+
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+
+		return sum;
 	}
 
 	@Override
